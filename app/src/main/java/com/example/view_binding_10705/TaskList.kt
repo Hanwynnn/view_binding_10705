6c99a640daca37488c08b1905803a5628698164a
Hanwynnn <tanghanwyn@gmail.com>
2022-09-15 08:30:19 +0700

    View_Binding_10705

---
+++ app/src/main/java/com/example/view_binding_10705/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.view_binding_10705
+
+import kotlin.concurrent.timerTask
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk mmebuat hari makin indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat gacha Mama Yaemiko tercinta / keqing wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama ei?", "Buat adu mekanin showcase ayaka pyro"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buat pamer ke view showcase ayaka pyro c6 + benner c6")
+    )
+}
